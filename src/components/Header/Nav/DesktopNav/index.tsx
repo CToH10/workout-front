77123bb7f447ec88636ee9d6eecf026fe6f1d221
CToH10 <luis.ebn98@gmail.com>
2023-07-18 09:10:16 -0300

    feat: add desktop no user

---
+++ src/components/Header/Nav/DesktopNav/index.tsx
@@ -0,0 +1,8 @@
+export const DesktopNav = () => {
+  return (
+    <section className="flex items-center justify-between gap-3">
+      <button className="btn-medium btn-brand-opacity">Login</button>
+      <button className="btn-medium btn-brand-outline-light">Cadastrar</button>
+    </section>
+  );
+};
--- src/components/Header/Nav/index.tsx
+++ src/components/Header/Nav/index.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import { useWindowSize } from "@/utils/hooks/windowSize";
+import { DesktopNav } from "./DesktopNav";
 
 export const NavItems = () => {
   const size = useWindowSize();
 
   return (
-    <nav>
+    <nav className="w-1/5 flex items-center justify-center ">
       {size.width <= 430 ? (
         <>
           <p className="text-grey-10">{size.width}</p>
         </>
       ) : (
-        <p className="text-grey-10">{size.height}</p>
+        <DesktopNav />
       )}
     </nav>
   );
