fd9258ce5e114125406bb149ee7bf29ab1b2354b
CToH10 <luis.ebn98@gmail.com>
2023-07-18 10:53:49 -0300

    feat: add login context

---
+++ src/context/apiContext.tsx
@@ -0,0 +1,44 @@
+import { api } from "@/service/api";
+import { AxiosError } from "axios";
+import router from "next/router";
+import { parseCookies, setCookie } from "nookies";
+import { ReactNode, createContext, useState } from "react";
+import { toast } from "react-toastify";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface ApiProviderData {
+  login: (userData: any) => Promise<void>;
+}
+
+export const ApiContext = createContext<ApiProviderData>({} as ApiProviderData);
+
+export function ApiProvider({ children }: Props) {
+  const [token, setToken] = useState<string | undefined>(
+    parseCookies(null, "workoutManager.token")["workoutManager.token"]
+  );
+
+  const login = async (userData: any) => {
+    try {
+      const data = await api.post("login", userData);
+
+      setCookie(null, "workoutManager.token", data.data, {
+        maxAge: 60 * 30,
+        path: "/",
+      });
+
+      setToken(data.data);
+
+      router.push("/");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(`${error.response?.data.message}`);
+        console.log(error);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+}
