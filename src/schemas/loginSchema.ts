f08fe93de5cd449b8014822db77a109baae169fa
CToH10 <luis.ebn98@gmail.com>
2023-09-28 11:46:53 -0300

    fix: empty password message

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email().nonempty(),
-  password: z.string().nonempty(),
+  password: z.string().nonempty("Password must not be empty"),
 });
 
 export type TLogin = z.infer<typeof loginSchema>;
