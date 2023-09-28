3f3bdb4b925969718420ada2f22121bc641f1b6c
CToH10 <luis.ebn98@gmail.com>
2023-09-28 11:52:20 -0300

    fix: header navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import { NavItems } from "./Nav";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="flex justify-between items-center w-4/5 absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-9 drop-shadow box-shadow-white border-b-2 border-b-grey-9 pb-2 min-h-[54px]">
-      <h1 className="text-grey-10 font-extrabold">Workout Manager</h1>
+      <Link className="text-grey-10 font-extrabold" href="/">
+        Workout Manager
+      </Link>
       <NavItems />
     </header>
   );
