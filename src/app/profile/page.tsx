1425efd4858386b8b10410048a97d82b48aefbde
CToH10 <luis.ebn98@gmail.com>
2023-10-23 12:24:02 -0300

    fix: workout list position on profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -17,7 +17,7 @@ export default function Profile() {
     <>
       <UserCard className="absolute top-[134px] -translate-x-2/4 -translate-y-2/4 left-2/4" />
       <EditOrDelete className="absolute top-[185px] left-2/4 -translate-x-2/4 translate-y-2/4" />
-      <WorkoutList className="absolute top-[15px] left-1/4 -translate-x-1/4 translate-y-2/4" />
+      <WorkoutList className="absolute top-[15px] left-1/4 translate-x-[0] translate-y-[15%] min-h-[1650px] max-h-[1650px]" />
     </>
   );
 }
