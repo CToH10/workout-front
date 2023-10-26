e521424b501f00fe243c3d12ddcc9953a00fe2bc
CToH10 <luis.ebn98@gmail.com>
2023-10-26 14:50:56 -0300

    feat: edit workout on page

--- src/app/workout/[workoutId]/page.tsx
+++ src/app/workout/[workoutId]/page.tsx
@@ -4,6 +4,7 @@ import { WorkoutToEditCard } from "@/components/WorkoutToEditCard";
 import { useApi } from "@/context/apiContext";
 import { getWorkoutDate } from "@/utils/workoutDate";
 import { useEffect } from "react";
+import { FaPen, FaTrash } from "react-icons/fa6";
 
 export default function EditWorkout({
   params,
@@ -20,9 +21,15 @@ export default function EditWorkout({
 
   return (
     <section className="absolute top-[185px] left-2/4 -translate-x-2/4 -translate-y-[25%] max-h-[360px] min-h-[360px] max-w-[300px] lg:max-w-[770px]">
-      <h1 className="text-heading1 text-grey-10">
-        {getWorkoutDate(workoutToPage?.date!)}
-      </h1>
+      <section className="flex justify-between items-center mb-4 max-w-[340px] min-[1408px]:max-w-none">
+        <h1 className="text-heading1 text-grey-10">
+          {getWorkoutDate(workoutToPage?.date!)}
+        </h1>
+        <section className="buttons flex justify-between gap-4">
+          <button className="btn-medium btn-brand1">{<FaPen />}</button>
+          <button className="btn-medium btn-alert">{<FaTrash />}</button>
+        </section>
+      </section>
       <ul className="flex flex-wrap gap-6">
         {workoutToPage?.daily_exercise.map((workout) => {
           return (
